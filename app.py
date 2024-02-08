560c4c3120f2aaea350ecc4b4b5a97b1748c0004
kelligriffin3 <kelligriffin2025@u.northwestern.edu>
2024-02-08 16:28:48 -0600

    add and clear comments

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
-from data_handler import test_data_handler
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-curr_comments = ["Sample Comment 1", "Sample Comment 2", "Sample Comment 3"]
+curr_comments = []
 
 @app.route('/')
 def home():
-    # data = test_data_handler()
     return render_template('BeeNice.html', comments=curr_comments)
+
+@app.route('/add_comment', methods=['POST'])
+def add_comment():
+    data = request.get_json()
+    new_comment = data['comment']
+    curr_comments.append(new_comment)
+    return jsonify({'message': 'Comment added successfully'}), 200
+
+@app.route('/clear_comments', methods=['DELETE'])
+def clear_comments():
+    data = request.get_json()
+    curr_comments.clear()
+    return jsonify({'message': 'Comment added successfully'}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,24 @@
+function postComment() {
+    var commentInput = document.getElementById("commentInput").value;
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/add_comment", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(JSON.stringify({ comment: commentInput }));
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        location.reload(); // Reload the page to display updated comments
+      }
+    };
+  }
+
+function clearComments() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("DELETE", "/clear_comments", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(JSON.stringify({ comment: "clear" }));
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        location.reload(); // Reload the page to display updated comments
+      }
+    };
+  }
