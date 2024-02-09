9f774394a4cfd5f1dcf7adaa251cf9cc9b222905
kelligriffin3 <kelligriffin2025@u.northwestern.edu>
2024-02-09 12:19:42 -0600

    add sentiment analysis

--- app.py
+++ app.py
@@ -1,18 +1,34 @@
 from flask import Flask, render_template, request, jsonify
+from data_handler import is_nice, get_alt_comments
 
 app = Flask(__name__)
 
 curr_comments = []
+alt_comments = []
+curr_sentiment = None # True if nice, False if mean
 
 @app.route('/')
 def home():
-    return render_template('BeeNice.html', comments=curr_comments)
+    #comms = test_data_handler(curr_comments)
+    return render_template('BeeNice.html', comments=curr_comments, curr_sentiment=curr_sentiment, alt_comments=alt_comments)
 
 @app.route('/add_comment', methods=['POST'])
 def add_comment():
+    global curr_sentiment
+    global alt_comments
+
     data = request.get_json()
     new_comment = data['comment']
-    curr_comments.append(new_comment)
+
+    # Check comment sentiment here!
+    curr_sentiment = is_nice(new_comment)
+    
+    if not curr_sentiment:
+        alt_comments = get_alt_comments(new_comment)
+    else:
+        # only post new comment if not mean
+        curr_comments.append(new_comment)
+
     return jsonify({'message': 'Comment added successfully'}), 200
 
 @app.route('/clear_comments', methods=['DELETE'])
